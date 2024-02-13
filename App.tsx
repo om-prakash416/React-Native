7aa8582e79fd1269ee3177d2bf909775a8cd0948
Om Prakash <omprakash31181@gmail.com>
2024-02-13 19:49:20 +0530

    react practice

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }} >Hello React Native</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }} >Hello</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }} >like</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }} >subscribe</Text>
+      <Text style={{fontSize:30, fontWeight:'bold'}} >Op_Ydv</Text>
+    </>
+  );
+};
+
+
+
+export default App;
